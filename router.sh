cd550a44377c285299eb737a19863491704dc803
bunnycou <noahjcou@gmail.com>
2025-04-07 04:38:39 -0400

    added setup script for raspi

---
+++ router.sh
@@ -0,0 +1,13 @@
+# script to be ran on raspberry pi
+# make sure system is up to date
+sudo apt update -y
+sudo apt upgrade -y
+# install prereqs
+sudo apt install procps iproute2 dnsmasq iptabels hostapd iw haveged
+# install lnxrouter
+sudo curl -o /usr/local/bin/lnxrouter https://raw.githubusercontent.com/bunnycou/linux-router/refs/heads/master/lnxrouter
+# create run script
+echo "sudo lnxrouter --ap wlan0 TriviaSync -p password -i eth0 -g 192.168.12.1 -d" > router
+chmod +x router
+# add entry to hosts file for dns
+echo "192.168.12.12 triviasync.com www.triviasync.com" | sudo tee -a /etc/hosts
